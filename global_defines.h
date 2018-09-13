ecff833976b7921b3f0e2a8354800a615d870e28
bassirou ngom <ibrahimayengue90@gmail.com>
2018-09-13 16:48:21 +0000

    mqtt-sn over lora

---
+++ global_defines.h
@@ -0,0 +1,30 @@
+
+#ifndef ARDUINO_MQTTSN_CLIENT_GLOBAL_DEFINES_H
+#define ARDUINO_MQTTSN_CLIENT_GLOBAL_DEFINES_H
+
+#include <stdint.h>
+
+struct device_address {
+    uint8_t bytes[6];  // mac
+    device_address() {
+        bytes[0] = 0x0;
+        bytes[1] = 0x0;
+        bytes[2] = 0x0;
+        bytes[3] = 0x0;
+        bytes[4] = 0x0;
+        bytes[5] = 0x0;
+    }
+};
+
+void printDeviceAddress(device_address *address) {
+    for (uint8_t i = 0; i < sizeof(device_address); i++) {
+        if (i == sizeof(device_address) - 1) {
+            Serial.print(address->bytes[i]);
+        } else {
+            Serial.print(address->bytes[i]);
+            Serial.print(", ");
+        }
+    }
+}
+
+#endif //ARDUINO_MQTTSN_CLIENT_GLOBAL_DEFINES_H
